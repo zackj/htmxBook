596bd51371b038444a0a5ef17b6123f9bfa02f91
zackj <zack@layeredlogic.com>
2026-01-11 11:24:47 -0700

    HTML_1 version of Contacts App

---
+++ HTML_1_app/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, redirect, render_template, flash
+from contacts_model import Contact
+
+app = Flask(__name__)
+app.secret_key = b"ZackWuzHere"
+
+Contact.load_db()
+
+
+@app.route("/")
+def index():
+    return redirect("/contacts")
+
+
+@app.route("/contacts")
+def contacts():
+    search = request.args.get("q")
+    print(f"Contacts {search=}")
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+    return render_template("index.html", contacts=contacts_set)
+
+
+@app.route("/contacts/<contact_id>")
+def contacts_view(contact_id=0):
+    contact = Contact.find(contact_id)
+    return render_template("show.html", contact=contact)
+
+
+@app.route("/contacts/new", methods=["GET"])
+def contacts_new_get():
+    return render_template("new.html", contact=Contact())
+
+
+@app.route("/contacts/new", methods=["POST"])
+def contacts_new():
+    c = Contact(
+        None,
+        request.form['first_name'],
+        request.form['last_name'],
+        request.form['phone'],
+        request.form['email']
+    )
+    if c.save():
+        flash("Created new Contact")
+        return redirect("/contacts")
+    else:
+        return render_template("new.html", contact=c)
+
+
+@app.route("/contacts/<contact_id>/edit", methods=["GET"])
+def contacts_edit_get(contact_id=0):
+    contact = Contact.find(contact_id)
+    return render_template("edit.html", contact=contact)
+
+
+@app.route("/contacts/<contact_id>/edit", methods=["POST"])
+def contacts_edit_post(contact_id=0):
+    c = Contact.find(contact_id)
+    c.update(
+        request.form['first_name'],
+        request.form['last_name'],
+        request.form['phone'],
+        request.form['email']
+    )
+    if c.save():
+        flash("Updated Contact")
+        return redirect(f"/contacts/{str(c.id)}")
+    else:
+        return render_template("edit.html", contact=c)
+
+
+@app.route("/contacts/<contact_id>/delete", methods=["POST"])
+def contacts_delete(contact_id=0):
+    print(f"delete contact {str(contact_id)}")
+    c = Contact.find(contact_id)
+    c.delete()
+    flash("Deleted Contact!")
+    return redirect("/contacts")
